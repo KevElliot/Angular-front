e13c410da947f7d720ddb9021ad6a48572158f00
KevElliot <50426021+KevElliot@users.noreply.github.com>
2021-04-15 20:42:12 +0300

    update matiere lien pour heroku bug

--- src/app/shared/matiere.service.ts
+++ src/app/shared/matiere.service.ts
@@ -10,7 +10,8 @@ export class MatiereService {
 
   constructor(private loggingService:LoggingService, private http:HttpClient,private authService:AuthService) { }
 
-  uri = "http://localhost:8010/api/matiere";
+  //uri = "http://localhost:8010/api/matiere";
+  uri = "https://mbdsangularback.herokuapp.com/api/matiere"
   //uri = "https://backmadagascar2021.herokuapp.com/api/assignments"
 
   allMatiere(matiere:any):Observable<any> {
